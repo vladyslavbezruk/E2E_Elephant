6371246752f85f4956e01c9888179a08021d9511
vladyslavbezruk <vladyslavbezruk@yahoo.com>
2024-01-01 23:24:04 +0200

    Added LoginPage.

---
+++ src/test/java/com/e2e/e2e_elephant/LoginPage.java
@@ -0,0 +1,12 @@
+package com.e2e.e2e_elephant;
+
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.ElementsCollection;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+// page_url = http://127.0.0.1:7000/login
+public class LoginPage {
+    public SelenideElement inputFloating = $("#floatingInput");
+}
