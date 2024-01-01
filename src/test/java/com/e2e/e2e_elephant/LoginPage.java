4bc031b6d57a04af11d88be55717a2c0bf3fb462
vladyslavbezruk <vladyslavbezruk@yahoo.com>
2024-01-01 23:46:15 +0200

    Changed.

--- src/test/java/com/e2e/e2e_elephant/LoginPage.java
+++ src/test/java/com/e2e/e2e_elephant/LoginPage.java
@@ -9,4 +9,8 @@ import static com.codeborne.selenide.Selenide.*;
 // page_url = http://127.0.0.1:7000/login
 public class LoginPage {
     public SelenideElement inputFloating = $("#floatingInput");
+
+    public SelenideElement inputFloatingPassword = $("input[aria-label='Password']");
+
+    public SelenideElement linkPassShowHide = $("#pass-show-hide");
 }
