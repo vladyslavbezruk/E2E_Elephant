991df8441ee7fb2a71a96db5bebf058d36b2ab5d
vladyslavbezruk <vladyslavbezruk@yahoo.com>
2024-01-02 19:47:56 +0200

    Changed.

--- src/test/java/com/e2e/e2e_elephant/ResetPasswordPageToken.java
+++ src/test/java/com/e2e/e2e_elephant/ResetPasswordPageToken.java
@@ -14,4 +14,5 @@ public class ResetPasswordPageToken {
 
     public SelenideElement buttonChangePassword = $("button[class='btn']");
 
+    public SelenideElement divPasswordNotFoundValidation = $("div[class*='fade']");
 }
