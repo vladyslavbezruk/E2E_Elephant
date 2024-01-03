9e63488718fa89d5220fa600beedd0f373d0a349
Sonlich <sonlich11@gmail.com>
2024-01-03 23:33:30 +0200

    .add TS097_1

--- src/test/java/com/e2e/e2e_elephant/ProfilePage.java
+++ src/test/java/com/e2e/e2e_elephant/ProfilePage.java
@@ -13,4 +13,26 @@ public class ProfilePage {
     public SelenideElement buttonOutlinePrimary2 = $("html > body > div > div > main > div:nth-of-type(5) > div > div:nth-of-type(2) > form > div > div:nth-of-type(2) > button");
 
     public SelenideElement linkUkraine = $("html > body > div > div > main > div:nth-of-type(2) > a:nth-of-type(2)");
+
+    public SelenideElement h3UserPreset = $("html > body > div > div > main > div:nth-of-type(2) > h3");
+
+    public SelenideElement divUserPresetYourPrefered = $("html > body > div > div > main > div:nth-of-type(2)");
+
+    public SelenideElement linkEnglish = $("html > body > div > div > main > div:nth-of-type(2) > a:nth-of-type(1)");
+
+    public SelenideElement linkUkraine2 = $("html > body > div > div > main > div:nth-of-type(2) > a:nth-of-type(2)");
+
+    public SelenideElement h3ApiKeys = $("html > body > div > div > main > div:nth-of-type(3) > h3");
+
+    public SelenideElement labelPublicApiKey = $("label[for='public-api']");
+
+    public SelenideElement labelPrivateApiKey = $("label[for='private-api']");
+
+    public SelenideElement buttonGenerate = $("html > body > div > div > main > div:nth-of-type(3) > form > div:nth-of-type(2) > button");
+
+    public SelenideElement h3DatabaseUser = $("html > body > div > div > main > div:nth-of-type(4) > h3");
+
+    public SelenideElement buttonChangePassword = $("html > body > div > div > main > div:nth-of-type(4) > form > div:nth-of-type(2) > button");
+    
+    
 }
