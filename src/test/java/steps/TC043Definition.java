17ae1ae3aac414aff40779bf5847c8eea91762c7
vladyslavbezruk <vladyslavbezruk@yahoo.com>
2024-01-03 21:44:02 +0200

    Added TC043.

---
+++ src/test/java/steps/TC043Definition.java
@@ -0,0 +1,42 @@
+package steps;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.e2e.e2e_elephant.DatabasePage;
+import com.e2e.e2e_elephant.ProfilePage;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.screenshot;
+
+public class TC043Definition {
+    com.e2e.e2e_elephant.LoginPage loginPage = new com.e2e.e2e_elephant.LoginPage();
+    com.e2e.e2e_elephant.HomePage homePage = new com.e2e.e2e_elephant.HomePage();
+    ProfilePage profilePage = new ProfilePage();
+    DatabasePage databasePage = new DatabasePage();
+    String dbName = null;
+
+    @BeforeAll
+    public static void setUpAll() {
+        Configuration.browserSize = "1800x1000";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeEach
+    public void setUp() {
+        // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
+        Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
+    }
+
+    @Then("Підтвердіть видалення бази даних у спливаючому вікні.")
+    public void a() {
+        throw new AssertionError("Спливаючого вікна для підтвердження видалення немає");
+    }
+}
